4ba4a58166539d876cbde324e56c412f1fd3d3cb
Andrew <Andrew.lowther@senseye.io>
2017-11-22 12:46:31 -0500

    Formatting

--- layer.py
+++ layer.py
@@ -1,24 +1,25 @@
 """Layer class for making up a fully connected neural network"""
 import numpy as np
 
-#To choose: either use scipy for logistic, or numpy.tanh as activation
+
+# To choose: either use scipy for logistic, or numpy.tanh as activation
 class Layer(object):
     """Fully connected neural network using vectorized operations"""
     def __init__(self, size=1, inputs=1):
         """Initializer takes number of nodes and expected size of input"""
         self.size = size
         self.input = inputs
-        self.weights = np.ndarray((size,inputs))
+        self.weights = np.ndarray((size, inputs))
 
-    def apply(self, input):
-        """Computes final result for a given input"""
-        activations = self.weights @ input
+    def apply(self, sample):
+        """Computes final result for a given input sample"""
+        activations = self.weights @ sample
         return np.tanh(activations)
 
-    #TODO include mathematics for backpropagation
-    #consider reformatting to daisy chain and recurse, rather than iterate in neural net
+    # TODO include mathematics for backpropagation
+    # consider reformatting to daisy chain and recurse, rather than iterate in neural net
 
-    #TODO implement file serialization and deserialization
+    # TODO implement file serialization and deserialization
 
     def __str__(self):
         return str(self.weights)
--- neural_network.py
+++ neural_network.py
@@ -1,9 +1,10 @@
 """Neural Network class, currently only supports fully connected"""
 import numpy as np
-import layer
+import layer as fcl
 
-#currently just works for fully connected
-#if a different versin is created, this should really be a subclass
+
+# currently just works for fully connected
+# if a different version of a network is created, this should really be a subclass
 class NeuralNetwork(object):
     """General neural network class for back-propagation"""
     def __init__(self, layers):
@@ -11,26 +12,26 @@ class NeuralNetwork(object):
         try:
             self.size = len(layers)
             self.layers = layers
-            self.shape = tuple(map(lambda L: L.size, layers))
+            self.shape = tuple(map(lambda layer: layer.size, layers))
         except TypeError:
             self.size = 1
             self.layers = [layers]
-            self.shape = (layers.size)
+            self.shape = tuple(layers.size)
 
-    def apply(self, input):
+    def apply(self, sample):
         """Applies an input vector or matrix to the neural net and returns the result"""
-        #could be rewritten with reduce as functional
-        #probably not worthwhile
+        # could be rewritten with reduce as functional
+        # probably not worthwhile
         for layer in self.layers:
-            input = layer.apply(input)
-        return input
+            sample = layer.apply(sample)
+        return sample
 
-    def backprop(self, input, expected):
+    def backprop(self, sample, expected):
         """applies back-propagation for the network"""
-        result = self.apply(input)
-        #TODO write back-propagation for network
-        #consider case of using multiple exaples at once for vectoriization
+        result = self.apply(sample)
+        # TODO write back-propagation for network
+        # consider case of using multiple exaples at once for vectoriization
 
-    #TODO allow updating and iterative construction
-    #TODO add file saving and restoring
-    #TODO string and pretty printing functions
+    # TODO allow updating and iterative construction
+    # TODO add file saving and restoring
+    # TODO string and pretty printing functions
