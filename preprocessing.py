e73391857dbb6bf0c1f022df512b54ec2b42aaea
Andrew <Andrew.lowther@senseye.io>
2017-11-27 11:09:39 -0500

    reworked processing

--- preprocessing.py
+++ preprocessing.py
@@ -2,82 +2,53 @@ import scipy.io
 import os
 import numpy as np
 import pickle
+import glob
+
 np.set_printoptions(threshold=np.nan)
-dataDir = "dataset/" #put your directory here
+dataDir = "../Data/" #put your directory here
 
-def load_mat_to_numpy(filename):
-	"""
-	filename : str, file .mat with the data
-	return : if test or train file, the number of the dataset, the matrix (nb of antenna pairs*20)*nb of samples
-	"""
-	mat = scipy.io.loadmat(filename)
-	if 'train_feature_group_cell' in mat:
-		if name[16] == '.':
-			number_dataset = name[15]
-		else:
-			number_dataset = name[15:17]
-		train_data = mat['train_feature_group_cell']
-		train_data_fin = []
-		for i in range(len(train_data[0][0])):
-			train_data_reshape = np.zeros((len(train_data[0]), len(train_data[0][0][0])))
-			for j in range(len(train_data[0])):
-				train_data_reshape[j][:]= train_data[0][j][i]
-			train_data_fin.append(train_data_reshape)
-		return('train',number_dataset, train_data_fin)
-	else:
-		if name[15] == '.':
-			number_dataset = name[14]
-		else:
-			number_dataset = name[14:16]
-		test_data = mat['test_feature_group_cell']
-		print(test_data)
+def load_mat_to_numpy(files):
+    """
+    filename : str, file .mat with the data
+    return : if test or train file, the number of the dataset, the matrix (nb of antenna pairs*20)*nb of samples
+    """
+    train_mat = scipy.io.loadmat(files[0])['train_feature_group_cell']
+    test_mat = scipy.io.loadmat(files[1])['test_feature_group_cell']
 
-		test_data_fin = []
-		for i in range(len(test_data[0][0])):
-			test_data_reshape = np.zeros((len(test_data[0]), len(test_data[0][0][0])))
-			for j in range(len(test_data[0])):
-				test_data_reshape[j][:]= test_data[0][j][i]
-			test_data_fin.append(test_data_reshape)
-		return('test',number_dataset, test_data_fin)
+    num_end = files[0].find('.mat')
+    num_start = files[0].rfind('_')
+    number_dataset = int(files[0][num_start + 1:num_end])
 
-def normalization(dataset):
-	"""
-	normalize the dataset feature by feature
-	dataset : ndarray of size (nb of antenna pairs*20) 
-	return: dataset normalized
-	"""
-	mean = 0
-	#std = 0
-	for k in range(len(dataset[0])):
-		for i in range(len(dataset[0][0])):
-			mean = 0
-			std = 0
-			maxi = -10
-			for j in range(len(dataset)):
-				mean += dataset[j][k][i]	
-				maxi = max(maxi, abs(dataset[j][k][i])	)
-			mean /= len(dataset)
-			#for j in range(len(dataset)):
-			#	std += (dataset[j][k][i] - mean)**2
-			#std /= (len(dataset)-1)
-			#std = np.sqrt(std)
-			for j in range(len(dataset)):
-				dataset[j][k][i] -= mean
-				#dataset[j][k][i] /= std
-				dataset[j][k][i] /= abs(maxi)
-				dataset[j][k][i] = .9*dataset[j][k][i];
-	return(dataset)
-	
+    train_data = np.stack(*train_mat, axis=1)
+    test_data = np.stack(*test_mat, axis=1)
+    train_size = train_data.shape
+    train_data = train_data.reshape((train_size[0], train_size[1] * train_size[2]), order='C')
+    test_data = test_data.reshape((test_data.shape[0], train_size[1] * train_size[2]), order='C')
+
+    total_data = np.concatenate((train_data, test_data), axis=0)
+
+    feature_means = np.mean(total_data, axis=0)
+    total_data = total_data - feature_means
+    feature_max = np.max(np.abs(total_data), axis=0)
+    total_data = .9 * total_data / feature_max
+
+    train_data = total_data[:train_size[0]]
+    test_data = total_data[-test_data.shape[0]:]
+
+    return number_dataset, train_data, test_data
 
 if __name__ == '__main__':
-	for file in os.listdir( dataDir ) :
-		name = str(file)
+    test_files = glob.glob(dataDir + 'features_test_*')
+    train_files = glob.glob(dataDir + 'features_train_*')
+    filenames = zip(train_files, test_files)
+    for files in filenames :
+        # name = str(file)
 
-		typedata,nb, data = load_mat_to_numpy( dataDir+file )
-		number = int(nb)
-		data = normalization(data) #if you want to normalize the data
-		filename = typedata + '%s' %number +'.py'
-		print(filename)
-		with open(filename, mode='wb') as out:
-			pickle.dump(data, out)
-
+        nb, train, test = load_mat_to_numpy(files)
+        number = int(nb)
+        test_filename = '../PythonData/test' + str(number) + '.pck'
+        with open(test_filename, mode='wb') as out:
+            pickle.dump(test, out)
+        train_filename = '../PythonData/train' + str(number) + '.pck'
+        with open(train_filename, mode='wb') as out:
+            pickle.dump(train, out)
