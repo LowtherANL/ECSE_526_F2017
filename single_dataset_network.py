ba5d5e6dd57d9e5cef5a1277155626b519c7f05e
Andrew <Andrew.lowther@senseye.io>
2017-11-28 10:32:08 -0500

    Refactor

--- single_dataset_network.py
+++ single_dataset_network.py
@@ -19,8 +19,8 @@ def error(input, output, axis):
     return np.sum(np.power(input - output, 2), axis=axis)
 
 
-if __name__ == '__main__':
-    train, test = load_data(DEFAULT_SET)
+def construct_network(dataset):
+    train, test = load_data(dataset)
     layer_shapes = [train.shape[0]]
     layer_shapes.append(int(layer_shapes[0] / 2))
     layer_shapes.append(int(layer_shapes[1] / 5))
@@ -29,27 +29,29 @@ if __name__ == '__main__':
     L1 = layer.Layer(size=layer_shapes[0], inputs=layer_shapes[0])
     L1.step = 0.00000001
     layers = [L1]
-    for i in range(1,len(layer_shapes)):
+    for i in range(1, len(layer_shapes)):
         L = layer.Layer(size=layer_shapes[i], inputs=layer_shapes[i - 1])
-        L.step = L.step / (10 ** (6-i))
+        L.step = L.step / (10 ** (6 - i))
         layers[-1].next = L
         layers.append(L)
-    for j in range(50):
-        L1.back_propagate(train, train)
-    evaluation = L1.apply_chain(test)
-    # verification = np.max(evaluation, axis=0) - np.min(evaluation, axis=0)
-    result = error(evaluation, test, axis=0)
+    return L1, train, test
 
-    # print('check eval: ', verification)
+
+def train_network(NN, data, iterations):
+    for i in range(iterations):
+        NN.back_propagate(data, data)
+    return NN
+
+
+def test_network(NN, test, train):
+    evaluation = NN.apply_chain(test)
+    result = error(evaluation, test, axis=0)
     print('result errors: ', result)
-    # print('\n', evaluation.transpose()[:,0:5])
-    # for l in layers:
-    #     print(np.max(np.abs(l.weights)))
     # TODO improve the statistical analysis
     tumors = result[[8, 10, 12, 14]]
     healthy = result[[1, 2, 3, 4, 5, 6, 7, 9, 11, 13, 15]]
 
-    train_result = error(L1.apply_chain(train), train, axis=0)
+    train_result = error(NN.apply_chain(train), train, axis=0)
     threshold = np.max(train_result)
 
     min_tumor = np.min(tumors)
@@ -60,3 +62,9 @@ if __name__ == '__main__':
     fp = np.count_nonzero(healthy >= threshold)
     fn = np.count_nonzero(tumors <= threshold)
     print('realistic fn, fp', fn, fp)
+
+
+if __name__ == '__main__':
+    NN, training, testing = construct_network(DEFAULT_SET)
+    train_network(NN, training, 25)
+    test_network(NN, testing, training)
