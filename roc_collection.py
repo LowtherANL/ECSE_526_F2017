061b917b65b79c0dfb2ab5fef8f7ce697751462d
Andrew <Andrew.lowther@senseye.io>
2017-12-04 21:40:59 -0500

    Roc cleaned

--- roc_collection.py
+++ roc_collection.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as mplot
 
 
 class Collector(object):
+    """Object to add updates for ROC curves while testing is happening"""
     def __init__(self, filename, samples=54):
+        """Initialized with the provided file name as the output file"""
         self.file = open(filename, mode='w')
         self.file.write('dataset,')
         for i in range(samples):
@@ -12,6 +14,7 @@ class Collector(object):
         self.file.write('\n')
 
     def get_roc(self, train, test, tumors):
+        """Computes ROC values from input data"""
         total = np.concatenate((train, test), axis=0)
         threshes = np.sort(total)[::-1]
         positive_rates = []
@@ -20,20 +23,24 @@ class Collector(object):
         return positive_rates
 
     def write_roc(self, dataset, rates):
+        """Writes data to opened file"""
         self.file.write(str(dataset) + ',')
         for r in rates:
             self.file.write(str(r) + ',')
         self.file.write('\n')
 
     def close(self):
+        """Close open file handle"""
         self.file.close()
 
 
 class Tabulator(object):
+    """Class for reading files and producing associated values and graphs"""
     def __init__(self, filename):
         self.set_roc = {}
         points = 0
         self.points = None
+        # Read data from the provided file
         with open(filename, mode='r') as data_file:
             reader = csv.DictReader(data_file)
             for row in reader:
@@ -44,9 +51,8 @@ class Tabulator(object):
                 index = int(row['dataset'])
                 self.set_roc[index] = {}
                 for k in list(row.keys())[1:-1]:
-                #    if k == 'dataset':
-                #        continue
                     self.set_roc[index][float(k)] = (float(row[k]))
+        # Ensure proper ordering of points
         roc_m = np.ndarray((len(self.set_roc), points))
         for i, data in enumerate(self.set_roc.values()):
             for j, val in enumerate(sorted(data.keys())):
@@ -54,10 +60,12 @@ class Tabulator(object):
         self.roc = np.mean(roc_m, axis=0)
 
     def plot_roc(self):
+        """Create a plot of the given ROC"""
         mplot.plot(self.points, self.roc)
         mplot.show()
 
     def auc(self):
+        """Compute the associated AUC"""
         step = 1/len(self.points)
         return np.sum(step * self.roc)
 
@@ -68,11 +76,11 @@ class Tabulator(object):
         return str(self.roc)
 
 
+# After a mistake in the ROC collection, a repair file was needed to convert old, incorrect results to new, correct ones
 def convert_old_file(filename, suffix='-new', output=None):
     with open(filename, mode='r') as old:
         reader = csv.reader(old)
         header = reader.__next__()
-        # header = [header[0], *header[:1:-1]]
         data = []
         indices = []
         for i, row in enumerate(reader):
@@ -94,8 +102,10 @@ def convert_old_file(filename, suffix='-new', output=None):
                 new.write('\n')
 
 
+# If run alone, will collect the data for the given file, and display results
+# The commented conversion will also apply the update to older data
 if __name__ == '__main__':
-    convert_old_file('first_test_tanh.csv')
+    # convert_old_file('first_test_tanh.csv')
     tab = Tabulator('first_test_tanh-new.csv')
     print(tab)
     print(len(tab.roc))
